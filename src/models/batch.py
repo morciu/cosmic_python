fbdf6d5bf60f9816354683569fa0476b4976ceb6
morciu <marianpralea@gmail.com>
2023-09-17 11:03:32 +0300

    Create can_allocate method for Batch model

--- src/models/batch.py
+++ src/models/batch.py
@@ -7,9 +7,12 @@ from src.models.order_line import OrderLine
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
-        self.sku = (sku,)
-        self.eta = (eta,)
+        self.sku = sku
+        self.eta = eta
         self.available_quantity = qty
 
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
