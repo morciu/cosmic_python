3ebe6e08c3c5c4f045bbefbfdbb69b9beea8ea8e
morciu <marianpralea@gmail.com>
2023-09-17 10:54:16 +0300

    Create unit tests for validating stock allocation

---
+++ tests/fixtures/make_batch_and_line.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+from src.models.batch import Batch
+from src.models.order_line import OrderLine
+
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty),
+    )
